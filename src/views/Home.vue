0bed4f9df5ea1e94c9dcfbc4407c2a56d3510d08
Kanto Kanto72 <kanto72@yandex.ru>
2021-10-25 09:32:21 +0500

    todo-start

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="logo">ToDo List</div>
+      <div class="form">
+        <input type="text" class="text">
+        <button class="btn">Add a new task</button>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+    <h2>
+      <span>To Do</span>
+      <span class="mask-num">10</span>
+    </h2>
+    <ul class="mask-list">
+      <li>
+        <div>
+          <input type="checkbox">
+          <span>New video</span>
+        </div>
+        <button class="btn-remove">Remove</button>
+      </li>
+    </ul>
+    <h2>
+      <span>Done</span>
+      <span class="mask-num">10</span>
+    </h2>
+    <ul class="mask-list complete-list">
+      <li>
+        <div>
+          <input type="checkbox" checked>
+          <span>Add code</span>
+        </div>
+        <button class="btn-remove">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
